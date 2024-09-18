aaade229b164400964fae936509d27cc37d79f54
Sharif <mdsh8692@gmail.com>
2024-09-18 21:43:58 +0600

    initial setup

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+document.getElementById('login-btn').addEventListener('click', function(e){
+    e.preventDefault()
+   
+    
+})
+
+const phoneNum = document.getElementById('phone-number')
+const pinNum = document.getElementById('pin-number')
+
+
